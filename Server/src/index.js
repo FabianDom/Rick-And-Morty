1043db22e585060f8b9f766277331d6369df053b
FabianDom <fabiandmz13@gmail.com>
2023-05-30 15:10:01 -0500

    Server

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,6 +3,6 @@ const { conn } = require("./DB_connection");
 const PORT = 3001;
 
 server.listen(PORT, async () => {
-  await conn.sync({ force: true });
+  await conn.sync({ force: false });
   console.log("Server raised in port: " + PORT);
 });
